26e8de17a54ddaa7c3d670e2be7a7b8fa00a6461
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-18 22:11:25 +0200

    aggiunto compose senza kafka per deploy veloci

--- kafka-producer/entrypoint.sh
+++ kafka-producer/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+until curl -s $GC25_API_ENDPOINT >/dev/null; do
+  sleep 2
+done
 java -jar /opt/app/app.jar "$@"
--- scripts/build-kafka-client.sh
+++ scripts/build-kafka-client.sh
@@ -1,5 +1,5 @@
-mkdir -p kafka-client/target
+mkdir -p kafka-producer/target
 git clone git@github.com:GentiliEmanuele/batches_producer.git
 mvn clean install -f batches_producer/pom.xml
-cp batches_producer/target/*-with-dependencies.jar kafka-client/target/app.jar
+cp batches_producer/target/*-with-dependencies.jar kafka-producer/target/app.jar
 rm -rf ./batches_producer

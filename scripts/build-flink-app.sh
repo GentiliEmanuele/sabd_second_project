a089b29e17e0076ca8f8cdbb3bac2233305cca45
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-24 00:44:31 +0200

    rimosso kafka

--- scripts/build-flink-app.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p target
-mvn clean install -f /home/francesco/Documents/Università/SABD/Progetti/2/progetto2/pom.xml
-cp /home/francesco/Documents/Università/SABD/Progetti/2/progetto2/target/progetto2-1.0-SNAPSHOT.jar target
--- scripts/build-kafka-client.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p kafka-producer/target
-git clone git@github.com:GentiliEmanuele/batches_producer.git
-mvn clean install -f batches_producer/pom.xml
-cp batches_producer/target/*-with-dependencies.jar kafka-producer/target/app.jar
-rm -rf ./batches_producer

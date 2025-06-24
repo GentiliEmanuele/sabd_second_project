e40df923168c25429255b378ec820ea904a3fb6a
GentiliEmanuele <egentili2000@gmail.com>
2025-06-24 18:41:14 +0200

    Added Spark container TODO(try to submit a job)

--- kafka-producer/build.sh
+++ kafka-producer/build.sh
@@ -1,5 +1,5 @@
-mkdir target
-cd target
+mkdir kafka-target
+cd kafka-target
 git clone git@github.com:GentiliEmanuele/batches_producer.git
 mvn clean install -f batches_producer/pom.xml
 cp batches_producer/target/batches_producer-1.0-SNAPSHOT-jar-with-dependencies.jar ./app.jar
---
+++ spark/entrypoint.sh
@@ -0,0 +1,4 @@
+/opt/spark/bin/spark-class org.apache.spark.deploy.master.Master
+
+java -jar /opt/app/app.jar "$@"
+

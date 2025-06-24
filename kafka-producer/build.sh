62695496f6eaba94c722d304bc91fc5a82554470
GentiliEmanuele <egentili2000@gmail.com>
2025-06-24 17:09:30 +0200

    Added kafka producer

---
+++ kafka-producer/build.sh
@@ -0,0 +1,6 @@
+mkdir target
+cd target
+git clone git@github.com:GentiliEmanuele/batches_producer.git
+mvn clean install -f batches_producer/pom.xml
+cp batches_producer/target/batches_producer-1.0-SNAPSHOT-jar-with-dependencies.jar ./app.jar
+rm batches_producer -rf

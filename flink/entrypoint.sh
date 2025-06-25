b52c9cb6eb0c0d200d96529d8175225fa9f89693
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-25 18:20:17 +0200

    aggiunte variabili per controllare parallelismo e netem

--- flink/entrypoint.sh
+++ flink/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "[INFO] applying delay 50ms±5ms (normal distribution)"
-tc qdisc add dev eth0 root netem delay 50ms 5ms distribution normal
+if [ "${USE_NETEM:-}" = "true" ]; then
+    echo "[INFO] applying delay 50ms±5ms (normal distribution)"
+    tc qdisc add dev eth0 root netem delay 50ms 5ms distribution normal
+else
+    echo "[INFO] netem is disabled"
+fi
 
 exec /docker-entrypoint.sh "$@"

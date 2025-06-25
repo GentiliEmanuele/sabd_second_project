d04403318bf7711bb490686316efaba25835df5f
GentiliEmanuele <egentili2000@gmail.com>
2025-06-25 21:43:39 +0200

    Modified command that submit Spark's job

--- scripts/submit-spark-job.sh
+++ scripts/submit-spark-job.sh
@@ -1,1 +1,1 @@
-docker exec spark-master /opt/spark/bin/spark-submit --class it.sabd2425.spark.SparkStreamingApp --packages org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0 /opt/app/app.jar
+docker exec spark-master /opt/spark/bin/spark-submit --class it.sabd2425.spark.SparkStreamingApp /opt/app/app.jar

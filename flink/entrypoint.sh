e47e34fb7aceec7348a8ae6ff6fdde867f870aee
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-27 10:10:31 +0200

    configurazione abbastanza buona di flink

--- flink/entrypoint.sh
+++ flink/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "${USE_NETEM:-}" = "true" ]; then
     echo "[INFO] applying delay 50ms±5ms (normal distribution)"
-    tc qdisc add dev eth0 root netem delay 50ms 5ms distribution normal
+    tc qdisc add dev eth0 root netem delay 10ms 2ms distribution normal
 else
     echo "[INFO] netem is disabled"
 fi

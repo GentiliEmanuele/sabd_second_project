93d25c2636c07fcc918a126974758ec0d72a1462
GentiliEmanuele <egentili2000@gmail.com>
2025-07-02 14:36:11 +0200

    Modified some configuration. Modified the spark submit command

--- scripts/submit-spark-job.sh
+++ scripts/submit-spark-job.sh
@@ -1,1 +1,1 @@
-docker exec spark-master /opt/spark/bin/spark-submit --class it.sabd2425.spark.SparkStreamingApp /opt/app/app.jar
+docker exec spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --class it.sabd2425.spark.SparkStreamingApp /opt/app/app.jar

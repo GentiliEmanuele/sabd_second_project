ee1c7b1ba3152ab0e410689744e6dd12f6c06ab8
Francesco Donnini <donnini.francesco00@gmail.com>
2025-07-03 13:23:28 +0200

    separati parametri per il parallelismo da quelli di configurazione per flink (così è possibile specificare più configurazioni per il parallelismo)

--- flink/entrypoint.sh
+++ flink/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "${USE_NETEM:-}" = "true" ]; then
-    echo "[INFO] applying delay 50ms±5ms (normal distribution)"
+    echo "[INFO] applying delay 10ms±2ms (normal distribution)"
     tc qdisc add dev eth0 root netem delay 10ms 2ms distribution normal
 else
     echo "[INFO] netem is disabled"

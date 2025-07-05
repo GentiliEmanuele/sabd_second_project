2ac4ff97ffe858c334af1ff449a0c992c77c581c
GentiliEmanuele <egentili2000@gmail.com>
2025-07-05 13:00:36 +0200

    Modified script that merge Spark output

--- scripts/merge-spark-output.sh
+++ scripts/merge-spark-output.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-output="merged.csv"
-first=1
+result_folder=.
+declare -A headers
+headers=( 
+  ["ranking"]="print_id,batch_id,tile_id,saturated,p1,d1,p2,d2,p3,d3,p4,d4,p5,d5"
+  ["saturated"]="print_id,batch_id,tile_id,saturated"
+  ["outliers"]="print_id,batch_id,tile_id,saturated,outliers"
+)
+folders=("ranking" "saturated" "outliers")
 
-for dir in task_*; do
-  if [ -d "$dir" ]; then
-    csvfile=$(find "$dir" -maxdepth 1 -name "*.csv" | head -n 1)
-    if [ -f "$csvfile" ]; then
-      cat "$csvfile" >> "$output"
+for folder in "${folders[@]}"; do
+    input_dir="$result_folder/$folder"
+    output_file="$result_folder/${folder}.txt"
+
+    if [ -d "$input_dir" ]; then
+        echo "Processing $folder..."
+        echo "${headers[$folder]}" > "$output_file"
+
+        # Unisce tutti i .txt del folder, ignorando eventuali file già uniti
+        find "$input_dir" -maxdepth 1 -type f -name "*.txt" ! -name "${folder}.txt" -exec cat {} + >> "$output_file"
+
+        echo "Merged output with header saved to $output_file"
+    else
+        echo "Directory $input_dir not found. Skipping."
     fi
-  fi
 done
 
-echo "File uniti in $output"
